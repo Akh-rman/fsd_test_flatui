6348eb5ac9c1d40bc02bd17122b7562f1c2d9986
Akh-rman <akh-rman@rambler.ru>
2017-09-13 00:14:00 +0300

    Инициализировал проект. Создал package.json. Добавил webpack и плагины для webpack. Добавил в .gitignore директорию node_modules. Создал конфигурационный файл webpack.config.js для webpack. Написал конфигурации для development и production.

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const babel = require('./webpack/bable');
+const devserver = require('./webpack/devserver');
+const css = require('./webpack/css');
+const cssMinify = require('./webpack/css.minify');
+const sass = require('./webpack/sass');
+const extractCSS = require('./webpack/css.extract');
+const fonts = require('./webpack/fonts');
+const images = require('./webpack/images');
+const uglifyJS = require('./webpack/js.uglify');
+const copyHTML = require('./webpack/html.copy');
+const jshint = require('./webpack/jshint');
+const PATHS = require('./webpack/path.config');
+
+const common = merge([
+    {
+        entry: {
+            'main': PATHS.source + '/app.js',
+            'main.min': PATHS.source + '/app.js',
+        },
+        output: {
+            path: PATHS.build,
+            filename: 'js/[name].js'
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: PATHS.source + '/index.html'
+            })
+        ]
+    },
+    images(),
+    jshint(),
+    babel(),
+    fonts()
+]);
+
+module.exports = function (env) {
+    if (env === 'production') {
+        return merge([
+            {
+                plugins: [
+                    new CleanWebpackPlugin([PATHS.build])   
+                ]
+            },
+            common,
+            extractCSS(),
+            copyHTML(),
+            cssMinify(),
+            uglifyJS()
+        ]);
+    }
+    if (env === 'development') {
+        return merge([
+            common,
+            devserver(),
+            sass(),
+            css()
+        ])
+    }
+};
